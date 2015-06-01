4c85c3e6afb8195af8fdbdc9da1c2b5d4419b3e9
Craig Chandler <craig.chandler@interdynamics.com>
2015-06-01 20:23:11 +0930

    Added EmployeeView.js and setup routing

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 var app = {
 
+    route: function() {
+        var hash = window.location.hash;
+        if (!hash) {
+            $('body').html(new HomeView(this.store).render().el);
+            return;
+        }
+        var match = hash.match(app.detailsURL);
+        if (match) {
+            this.store.findById(Number(match[1]), function(employee) {
+                $('body').html(new EmployeeView(employee).render().el);
+            });
+        }
+    },
+
 	showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -28,13 +42,15 @@ var app = {
                 $(event.target).removeClass('tappable-active');
             });
         }
+		$(window).on('hashchange', $.proxy(this.route, this));
     },
 
     initialize: function() {
         var self = this;
+		this.detailsURL = /^#employees\/(\d{1,})/;
 		this.registerEvents();
         this.store = new MemoryStore(function() {
-            $('body').html(new HomeView(self.store).render().el);
+            self.route();
         });
     }
 };
