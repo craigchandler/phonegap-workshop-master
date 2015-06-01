55a61d9ecdfa1d3c814bf7629b97184f73ae7769
Craig Chandler <craig.chandler@interdynamics.com>
2015-06-01 11:44:52 +0930

    render html using template

--- js/main.js
+++ js/main.js
@@ -1,21 +1,10 @@
 var app = {
 
     findByName: function() {
-        console.log('findByName');
+        var self = this;
         this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+            $('.employee-list').html(self.employeeLiTpl(employees));
         });
-    },
-
-    initialize: function() {
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 
 	showAlert: function (message, title) {
@@ -26,23 +15,9 @@ var app = {
         }
     },
 	
-	initialize: function() {
-        var self = this;
-        this.store = new MemoryStore(function() {
-            self.showAlert('Store Initialized', 'Info');
-        });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-	
 	renderHomeView: function() {
-        var html =
-                "<div class='header'><h1>Home</h1></div>" +
-                "<div class='search-view'>" +
-                "<input class='search-key'/>" +
-                "<ul class='employee-list'></ul>" +
-                "</div>"
-            $('body').html(html);
-            $('.search-key').on('keyup', $.proxy(this.findByName, this));
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 
     initialize: function() {
@@ -50,6 +25,8 @@ var app = {
         this.store = new MemoryStore(function() {
             self.renderHomeView();
         });
+		this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
     }
 };
 
